67cc621c859f832a96746817e82ac55712f9fa1e
Noah Murad <nmurad2@uwo.ca>
2017-06-28 12:54:54 -0700

    Sensitivity controls added to pause menu

---
+++ Assets/SensitivitySlider.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SensitivitySlider : Sensitivity {
+	private const string SLIDER_TEXT_PREFIX = "sensitivity ";
+
+	private Slider slider;
+	private Text sliderText;
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponentInChildren<Slider> ();
+		sliderText = GetComponentInChildren<Text> ();
+
+		slider.onValueChanged.AddListener (delegate {
+			updateSensitivity ();
+		});
+
+		float sensitivity = getSensitivity ();
+		setSliderText (sensitivity);
+		slider.value = sensitivity;
+	}
+
+	private void updateSensitivity(){
+		// convert slider value to sensitivity value
+		float sensitivity = slider.value;
+
+		// update display
+		setSliderText(sensitivity);
+
+		// update playerprefs and current sensitivity
+		setSensitivity(sensitivity);
+	}
+
+
+
+	private void setSliderText(float sensitivity){
+		sliderText.text = SLIDER_TEXT_PREFIX + ((int)sensitivity).ToString ();
+	}
+
+	/*
+	private float sliderToSensitivity(float x){
+		return x;
+
+		// it appears to be much nicer with a linear scale :'(
+		//return Mathf.Pow(SLIDER_SCALE, x * Mathf.Log(MAX_SENSITIVITY-MIN_SENSITIVITY)/Mathf.Log(SLIDER_SCALE)) 
+		//	+ MIN_SENSITIVITY;
+	}
+		
+	private float sensitivityToSlider(float y){
+		return y;
+	}
+	*/
+
+}
