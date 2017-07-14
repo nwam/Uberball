b466a3c80cae0ae8520510b2cb17c5112e49b83f
Noah Murad <nmurad2@uwo.ca>
2017-07-13 22:51:43 -0700

    Implement gravity-changing gate. Implementations need to be done to player controller as well

---
+++ Assets/GravityGate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityGate : Gate {
+
+	public Vector3 gravity;
+	
+	protected override void effect(GameObject obj) {
+		// disables all other gravity effects
+		GravityGate[] gravityGates = GameObject.FindObjectsOfType<GravityGate>();
+		foreach (GravityGate gravityGate in gravityGates) {
+			gravityGate.disableEffect (obj);
+			gravityGate.killGUI ();
+		}
+
+		// change gravity
+		Physics.gravity = gravity;
+	}
+
+	public override void disableEffect(GameObject obj) {
+		Physics.gravity = new Vector3(0, -9.8f, 0);
+	}
+
+	protected override void setType(){
+		type = "grvty";
+	}
+}
