489203617097cb4f084b456649e4d769681577a3
Noah Murad <noahmurad@hotmail.com>
2017-11-16 21:22:11 -0800

    Remove duplicates

--- Assets/scripts/Rewards/GoalUnlocker.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.SceneManagement;
-
-public class GoalUnlocker : Unlocker {
-	void Start() {
-		key = SceneManager.GetActiveScene ().name + "_complete";
-	}
-}
--- Assets/scripts/Rewards/Locks.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Locks : MonoBehaviour {
-	public static string LEVEL_BOBOMB_BATTLEFIELD = "unlock_bbb";
-	public static string CHAR_CANNONBALL = "unlock_cannonball";
-}
